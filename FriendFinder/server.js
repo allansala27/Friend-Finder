efd97c9e827a5760edd5e1608d2ae198e6193e1b
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-05-09 15:45:38 -0700

    updated files

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,18 +1,21 @@
-// Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+// dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
 
+// hold express in a variable
 var app = express();
-var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+// establish a port
+var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//App listener - shows that the connection has been made on the right port
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
